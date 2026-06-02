5400e4821aad3074cfcba36f74ef17656de6ead3
Bùi Nhật Đức Anh <124678780+buinhatducanh@users.noreply.github.com>
2026-06-02 15:19:29 +0700

    update

--- dist-electron/services/dev_log.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.devLog = exports.silent = void 0;
-/** Default: silent. Set DEV_LOG=1 to enable dev console.log output */
-exports.silent = process.env.DEV_LOG !== '1';
-const devLog = (...a) => { if (!exports.silent)
-    console.log(...a); };
-exports.devLog = devLog;
